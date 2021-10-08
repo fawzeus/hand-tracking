abe40a15bfb96e004c2342d9e00779b71d68a8b6
fawzirjili <rjilifawzi@gmail.com>
2021-10-08 14:51:15 +0100

    fix eating ram bug

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from time import clock
 from HAND_TRACKING_MODULE import HandTracker
 import cv2
-import pyautogui
 from math import sqrt
 import numpy as np
 import autopy
+import pyautogui
 
 
 def main():
@@ -16,18 +16,25 @@ def main():
     smoothing=5
     plocx,plocy=0,0
     clockx,clocky=0,0
+    tracker=HandTracker()
+    
+    TEXT_FACE = cv2.FONT_HERSHEY_DUPLEX
+    TEXT_SCALE = 1.5
+    TEXT_THICKNESS = 2
+    TEXT = "clicked"
     #print(w,h)
     while True:
         _,img=cap.read()
-        #img = cv2.flip(img,1)
-        tracker=HandTracker()
+        img = cv2.flip(img,1)
+        
         tracker.find_hands(img)
         li=tracker.findPosition(img,draw=False)
         if len(li):
-            cv2.rectangle(img,(zone,zone),(640-zone,480-zone),(230,255,0),2)
+            #cv2.rectangle(img,(zone,zone),(640-zone,480-zone),(230,255,0),2)
             #print(li[4])
             _,x4,y4=li[4]
             _,x8,y8=li[8]
+            _,x12,y12=li[12]
             if li[8][2] < li[6][2]:
                 xmouse=np.interp(x8,(zone,640-zone),(0,w))
                 ymouse=np.interp(y8,(zone,480-zone),(0,h))
@@ -37,12 +44,24 @@ def main():
 
 
                 dist = sqrt((x4-x8)*(x4-x8) + (y4-y8)*(y4-y8))
-                print(dist)
-                autopy.mouse.move(w-clockx,clocky)
+                dist1 = sqrt((x12-x4)*(x12-x4) + (y12-y4)*(y12-y4))
+                pos2=y12
+                #print("dist1 =",dist1)
+                #print("dist =",dist)
+                autopy.mouse.move(clockx,clocky)
                 cv2.circle(img,(x8,y8),15,(230,255,0),cv2.FILLED)
                 plocx,plocy=clockx,clocky
-                if dist<20:
+                if dist<30:
                     autopy.mouse.click()
+                    cv2.circle(img,(x8,y8),15,(0,0,255),cv2.FILLED)
+                    cv2.putText(img, TEXT, (x8,y8), TEXT_FACE, TEXT_SCALE, (127,255,127), TEXT_THICKNESS, cv2.LINE_AA)
+                    print("clicked")
+                if dist1<110:
+                    #print("scroll ",(-10))
+                    pyautogui.scroll(-50)
+
+                
+
         cv2.imshow("Live",img)
         key=cv2.waitKey(1)
         if key==ord("q"):
